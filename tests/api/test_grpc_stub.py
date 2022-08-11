520456b92f3ee9eb4fb407e400cbe7dcfcd579d1
Chad Retz <chad@temporal.io>
2022-08-11 15:13:45 -0500

    Test updates for operator and test service

--- tests/api/test_grpc_stub.py
+++ tests/api/test_grpc_stub.py
@@ -1,22 +1,46 @@
-import logging
-
+import google.protobuf.empty_pb2
+import google.protobuf.timestamp_pb2
 import grpc
 
 import temporalio
+import temporalio.api.operatorservice.v1
+import temporalio.api.testservice.v1
 import temporalio.api.workflowservice.v1
 
 
-class SimpleServer(temporalio.api.workflowservice.v1.WorkflowServiceServicer):
+class SimpleWorkflowServer(temporalio.api.workflowservice.v1.WorkflowServiceServicer):
     async def CountWorkflowExecutions(  # type: ignore # https://github.com/nipunn1313/mypy-protobuf/issues/216
         self,
         request: temporalio.api.workflowservice.v1.CountWorkflowExecutionsRequest,
         context: grpc.aio.ServicerContext,
     ) -> temporalio.api.workflowservice.v1.CountWorkflowExecutionsResponse:
-        logging.info("Server RPC called")
         assert request.namespace == "my namespace"
         assert request.query == "my query"
         return temporalio.api.workflowservice.v1.CountWorkflowExecutionsResponse(
             count=123
+        )
+
+
+class SimpleOperatorServer(temporalio.api.operatorservice.v1.OperatorServiceServicer):
+    async def DeleteNamespace(  # type: ignore # https://github.com/nipunn1313/mypy-protobuf/issues/216
+        self,
+        request: temporalio.api.operatorservice.v1.DeleteNamespaceRequest,
+        context: grpc.aio.ServicerContext,
+    ) -> temporalio.api.operatorservice.v1.DeleteNamespaceResponse:
+        assert request.namespace == "my namespace"
+        return temporalio.api.operatorservice.v1.DeleteNamespaceResponse(
+            deleted_namespace="my namespace response"
+        )
+
+
+class SimpleTestServer(temporalio.api.testservice.v1.TestServiceServicer):
+    async def GetCurrentTime(  # type: ignore # https://github.com/nipunn1313/mypy-protobuf/issues/216
+        self,
+        request: google.protobuf.empty_pb2.Empty,
+        context: grpc.aio.ServicerContext,
+    ) -> temporalio.api.testservice.v1.GetCurrentTimeResponse:
+        return temporalio.api.testservice.v1.GetCurrentTimeResponse(
+            time=google.protobuf.timestamp_pb2.Timestamp(seconds=123)
         )
 
 
@@ -26,20 +50,37 @@ async def test_python_grpc_stub():
     # Start server
     server = grpc.aio.server()
     temporalio.api.workflowservice.v1.add_WorkflowServiceServicer_to_server(
-        SimpleServer(), server
+        SimpleWorkflowServer(), server
+    )
+    temporalio.api.operatorservice.v1.add_OperatorServiceServicer_to_server(
+        SimpleOperatorServer(), server
+    )
+    temporalio.api.testservice.v1.add_TestServiceServicer_to_server(
+        SimpleTestServer(), server
     )
     port = server.add_insecure_port("[::]:0")
-    logging.info("Starting server on %s", port)
     await server.start()
 
     async with grpc.aio.insecure_channel(f"localhost:{port}") as channel:
-        stub = temporalio.api.workflowservice.v1.WorkflowServiceStub(channel)
-        response = await stub.CountWorkflowExecutions(
+        response = await temporalio.api.workflowservice.v1.WorkflowServiceStub(
+            channel
+        ).CountWorkflowExecutions(
             temporalio.api.workflowservice.v1.CountWorkflowExecutionsRequest(
                 namespace="my namespace", query="my query"
             )
         )
         assert response.count == 123
+        response = await temporalio.api.operatorservice.v1.OperatorServiceStub(
+            channel
+        ).DeleteNamespace(
+            temporalio.api.operatorservice.v1.DeleteNamespaceRequest(
+                namespace="my namespace"
+            )
+        )
+        assert response.deleted_namespace == "my namespace response"
+        response = await temporalio.api.testservice.v1.TestServiceStub(
+            channel
+        ).GetCurrentTime(google.protobuf.empty_pb2.Empty())
+        assert response.time.seconds == 123
 
-    logging.info("Stopping server")
     await server.stop(grace=None)
--- tests/test_service.py
+++ tests/test_service.py
@@ -1,44 +1,105 @@
 import inspect
 import os
 import re
-from typing import Dict, Tuple, Type
+from typing import Any, Callable, Dict, Mapping, Tuple, Type
 
+import google.protobuf.empty_pb2
+import google.protobuf.message
 import grpc
 
 import temporalio
+import temporalio.api.operatorservice.v1
+import temporalio.api.testservice.v1
 import temporalio.api.workflowservice.v1
 import temporalio.service
 from temporalio.client import Client
 
 
 def test_all_grpc_calls_present(client: Client):
-    # Collect workflow service calls
-    workflow_service_calls: Dict[str, Tuple[Type, Type]] = {}
-    for member in inspect.getmembers(client.workflow_service):
-        _, call = member
-        if isinstance(call, temporalio.service.ServiceCall):
-            workflow_service_calls[call.name] = (call.req_type, call.resp_type)
+    def assert_all_calls_present(
+        service: Any,
+        package: Any,
+        new_stub: Callable[[grpc.Channel], Any],
+        custom_req_resp: Mapping[
+            str,
+            Tuple[
+                Type[google.protobuf.message.Message],
+                Type[google.protobuf.message.Message],
+            ],
+        ] = {},
+    ) -> None:
+        # Collect service calls
+        service_calls: Dict[str, Tuple[Type, Type]] = {}
+        for _, call in inspect.getmembers(service):
+            if isinstance(call, temporalio.service.ServiceCall):
+                service_calls[call.name] = (call.req_type, call.resp_type)
 
-    # Collect gRPC service calls with a fake channel
-    channel = CallCollectingChannel()
-    temporalio.api.workflowservice.v1.WorkflowServiceStub(channel)
+        # Collect gRPC service calls with a fake channel
+        channel = CallCollectingChannel(package, custom_req_resp)
+        new_stub(channel)
 
-    assert channel.calls == workflow_service_calls
+        # Confirm they are the same
+        assert channel.calls == service_calls
+
+    assert_all_calls_present(
+        client.workflow_service,
+        temporalio.api.workflowservice.v1,
+        temporalio.api.workflowservice.v1.WorkflowServiceStub,
+    )
+    assert_all_calls_present(
+        client.operator_service,
+        temporalio.api.operatorservice.v1,
+        temporalio.api.operatorservice.v1.OperatorServiceStub,
+    )
+    assert_all_calls_present(
+        client.test_service,
+        temporalio.api.testservice.v1,
+        temporalio.api.testservice.v1.TestServiceStub,
+        {
+            # Abnormal req/resp
+            "GetCurrentTime": (
+                google.protobuf.empty_pb2.Empty,
+                temporalio.api.testservice.v1.GetCurrentTimeResponse,
+            ),
+            "SleepUntil": (
+                temporalio.api.testservice.v1.SleepUntilRequest,
+                temporalio.api.testservice.v1.SleepResponse,
+            ),
+            "UnlockTimeSkippingWithSleep": (
+                temporalio.api.testservice.v1.SleepRequest,
+                temporalio.api.testservice.v1.SleepResponse,
+            ),
+        },
+    )
 
 
 class CallCollectingChannel(grpc.Channel):
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        package: Any,
+        custom_req_resp: Mapping[
+            str,
+            Tuple[
+                Type[google.protobuf.message.Message],
+                Type[google.protobuf.message.Message],
+            ],
+        ],
+    ) -> None:
         super().__init__()
+        self.package = package
+        self.custom_req_resp = custom_req_resp
         self.calls: Dict[str, Tuple[Type, Type]] = {}
 
     def unary_unary(self, method, request_serializer, response_deserializer):
         # Last part after slash
         name = method.rsplit("/", 1)[-1]
-        req = getattr(temporalio.api.workflowservice.v1, name + "Request")
-        resp = getattr(temporalio.api.workflowservice.v1, name + "Response")
+        req_resp = self.custom_req_resp.get(name, None) or (
+            getattr(self.package, name + "Request"),
+            getattr(self.package, name + "Response"),
+        )
         # Camel to snake case
         name = re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
-        self.calls[name] = (req, resp)
+        self.calls[name] = req_resp
 
 
 CallCollectingChannel.__abstractmethods__ = set()
