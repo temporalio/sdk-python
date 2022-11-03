20dfe8e3c67619c116bf0fac8050af38139d4c82
Chad Retz <chad@temporal.io>
2022-11-03 12:42:18 -0500

    Add favicon to docs
    Fixes #140

---
+++ scripts/gen_docs.py
@@ -0,0 +1,17 @@
+import shutil
+import subprocess
+from pathlib import Path
+
+base_dir = Path(__file__).parent.parent
+
+if __name__ == "__main__":
+    print("Generating documentation...")
+
+    # Run pydoctor
+    subprocess.check_call("pydoctor")
+
+    # Copy favicon
+    shutil.copyfile(
+        base_dir / "scripts" / "_img" / "favicon.ico",
+        base_dir / "build" / "apidocs" / "favicon.ico",
+    )
