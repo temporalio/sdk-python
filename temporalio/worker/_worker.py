d667669ad4f9aaab5b0bf8111610b9b6491d7638
Radosław Gryta <radek.gryta@gmail.com>
2024-02-14 20:23:23 +0100

    Update temporalio/worker/_worker.py
    Co-authored-by: Chad Retz <chad.retz@gmail.com>

--- temporalio/worker/_worker.py
+++ temporalio/worker/_worker.py
@@ -633,7 +633,10 @@ def load_default_build_id(*, memoize: bool = True) -> str:
     # * Using the loader's get_code in rare cases can cause a compile()
 
     got_temporal_code = False
-    m = hashlib.md5(usedforsecurity=False)
+    if sys.version_info < (3, 9):
+        m = hashlib.md5()
+    else:
+        m = hashlib.md5(usedforsecurity=False)
     for mod_name in sorted(sys.modules):
         # Try to read code
         code = _get_module_code(mod_name)
