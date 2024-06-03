957958257a3c9ad7bb2eeb8aa4eef45f2498a8f6
Divyank Jain <divyank.jain@temporal.io>
2024-06-03 12:58:13 -0400

    Added integration test for external file, via added file (externalstacktrace.py).

---
+++ tests/helpers/externalstacktrace.py
@@ -0,0 +1,56 @@
+from datetime import timedelta
+import asyncio
+
+from temporalio import workflow, activity
+
+@workflow.defn
+class ExternalLongSleepWorkflow:
+    @workflow.run
+    async def run(self) -> None:
+        self._started = True
+        await asyncio.sleep(1000)
+
+    @workflow.query
+    def started(self) -> bool:
+        return self._started
+
+@activity.defn
+async def wait_cancel() -> str:
+    try:
+        if activity.info().is_local:
+            await asyncio.sleep(1000)
+        else:
+            while True:
+                await asyncio.sleep(0.3)
+                activity.heartbeat()
+        return "Manually stopped"
+    except asyncio.CancelledError:
+        return "Got cancelled error, cancelled? " + str(activity.is_cancelled())
+
+@workflow.defn
+class ExternalStackTraceWorkflow:
+    def __init__(self) -> None:
+        self._status = "created"
+
+    @workflow.run
+    async def run(self) -> None:
+        # Start several tasks
+        awaitables = [
+            asyncio.sleep(1000),
+            workflow.execute_activity(
+                wait_cancel, schedule_to_close_timeout=timedelta(seconds=1000)
+            ),
+            workflow.execute_child_workflow(
+                ExternalLongSleepWorkflow.run, id=f"{workflow.info().workflow_id}_child"
+            ),
+            self.never_completing_coroutine(),
+        ]
+        await asyncio.wait([asyncio.create_task(v) for v in awaitables])
+
+    async def never_completing_coroutine(self) -> None:
+        self._status = "waiting" # with external comment
+        await workflow.wait_condition(lambda: False)
+
+    @workflow.query
+    def status(self) -> str:
+        return self._status
