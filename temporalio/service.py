ee433e1a10f31acf005f12f573028f47e022f6b2
Dan Davison <dan.davison@temporal.io>
2024-10-10 12:46:03 -0400

    Bump version

--- temporalio/service.py
+++ temporalio/service.py
@@ -26,7 +26,7 @@ import temporalio.bridge.proto.health.v1
 import temporalio.exceptions
 import temporalio.runtime
 
-__version__ = "1.7.1"
+__version__ = "1.8.0"
 
 ServiceRequest = TypeVar("ServiceRequest", bound=google.protobuf.message.Message)
 ServiceResponse = TypeVar("ServiceResponse", bound=google.protobuf.message.Message)
