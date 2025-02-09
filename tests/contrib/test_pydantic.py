706f5f25d079e58ef7017fa46f33e7c1b761d4ca
Dan Davison <dan.davison@temporal.io>
2025-02-09 10:12:06 -0500

    Rename

--- tests/contrib/test_pydantic.py
+++ tests/contrib/test_pydantic.py
@@ -694,7 +694,7 @@ async def test_round_trip_pydantic_objects(client: Client):
     client = Client(**new_config)
     task_queue_name = str(uuid.uuid4())
 
-    orig_pydantic_objects = make_list_of_pydantic_objects()
+    orig_objects = make_list_of_pydantic_objects()
 
     async with Worker(
         client,
@@ -702,14 +702,14 @@ async def test_round_trip_pydantic_objects(client: Client):
         workflows=[RoundTripObjectsWorkflow],
         activities=[pydantic_models_activity],
     ):
-        round_tripped_pydantic_objects = await client.execute_workflow(
+        returned_objects = await client.execute_workflow(
             RoundTripObjectsWorkflow.run,
-            orig_pydantic_objects,
+            orig_objects,
             id=str(uuid.uuid4()),
             task_queue=task_queue_name,
         )
-    assert orig_pydantic_objects == round_tripped_pydantic_objects
-    for o in round_tripped_pydantic_objects:
+    assert returned_objects == orig_objects
+    for o in returned_objects:
         o._check_instance()
 
 
@@ -723,21 +723,21 @@ async def test_clone_objects_in_sandbox(client: Client):
     client = Client(**new_config)
     task_queue_name = str(uuid.uuid4())
 
-    orig_pydantic_objects = make_list_of_pydantic_objects()
+    orig_objects = make_list_of_pydantic_objects()
 
     async with Worker(
         client,
         task_queue=task_queue_name,
         workflows=[CloneObjectsWorkflow],
     ):
-        round_tripped_pydantic_objects = await client.execute_workflow(
+        returned_objects = await client.execute_workflow(
             CloneObjectsWorkflow.run,
-            orig_pydantic_objects,
+            orig_objects,
             id=str(uuid.uuid4()),
             task_queue=task_queue_name,
         )
-    assert round_tripped_pydantic_objects == orig_pydantic_objects
-    for o in round_tripped_pydantic_objects:
+    assert returned_objects == orig_objects
+    for o in returned_objects:
         o._check_instance()
 
 
@@ -786,17 +786,17 @@ async def test_complex_custom_type(client: Client):
         activities=[pydantic_models_activity],
     ):
         (
-            round_tripped_dataclass_objects,
-            round_tripped_pydantic_objects,
+            returned_dataclass_objects,
+            returned_pydantic_objects,
         ) = await client.execute_workflow(
             ComplexCustomTypeWorkflow.run,
             (orig_dataclass_objects, orig_pydantic_objects),
             id=str(uuid.uuid4()),
             task_queue=task_queue_name,
         )
-    assert orig_dataclass_objects == round_tripped_dataclass_objects
-    assert orig_pydantic_objects == round_tripped_pydantic_objects
-    for o in round_tripped_pydantic_objects:
+    assert orig_dataclass_objects == returned_dataclass_objects
+    assert orig_pydantic_objects == returned_pydantic_objects
+    for o in returned_pydantic_objects:
         o._check_instance()
 
 
@@ -845,25 +845,25 @@ async def test_complex_custom_union_type(client: Client):
         workflows=[ComplexCustomUnionTypeWorkflow],
         activities=[pydantic_models_activity],
     ):
-        round_tripped_objects = await client.execute_workflow(
+        returned_objects = await client.execute_workflow(
             ComplexCustomUnionTypeWorkflow.run,
             orig_objects,
             id=str(uuid.uuid4()),
             task_queue=task_queue_name,
         )
-    round_tripped_dataclass_objects, round_tripped_pydantic_objects = [], []
-    for o in round_tripped_objects:
+    returned_dataclass_objects, returned_pydantic_objects = [], []
+    for o in returned_objects:
         if isinstance(o, MyDataClass):
-            round_tripped_dataclass_objects.append(o)
+            returned_dataclass_objects.append(o)
         elif isinstance(o, BaseModel):
-            round_tripped_pydantic_objects.append(o)
+            returned_pydantic_objects.append(o)
         else:
             raise TypeError(f"Unexpected type: {type(o)}")
-    assert sorted(orig_dataclass_objects) == sorted(round_tripped_dataclass_objects)
+    assert sorted(orig_dataclass_objects) == sorted(returned_dataclass_objects)
     assert sorted(orig_pydantic_objects, key=lambda o: o.__class__.__name__) == sorted(
-        round_tripped_pydantic_objects, key=lambda o: o.__class__.__name__
+        returned_pydantic_objects, key=lambda o: o.__class__.__name__
     )
-    for o in round_tripped_pydantic_objects:
+    for o in returned_pydantic_objects:
         o._check_instance()
 
 
