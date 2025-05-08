aa749a5f7784dfca4b1dabbbd8b00832fe83beb1
Spencer Judge <spencer@temporal.io>
2025-05-08 15:33:09 -0700

    Use pytest exit code fix always

--- tests/conftest.py
+++ tests/conftest.py
@@ -140,18 +140,16 @@ async def worker(
     await worker.close()
 
 
-# There is an issue on 3.9 tests in GitHub actions where even though all tests
+# There is an issue in tests sometimes in GitHub actions where even though all tests
 # pass, an unclear outer area is killing the process with a bad exit code. This
 # hook forcefully kills the process as success when the exit code from pytest
 # is a success.
-if sys.version_info < (3, 10):
-
-    @pytest.hookimpl(hookwrapper=True, trylast=True)
-    def pytest_cmdline_main(config):
-        result = yield
-        if result.get_result() == 0:
-            os._exit(0)
-        return result.get_result()
+@pytest.hookimpl(hookwrapper=True, trylast=True)
+def pytest_cmdline_main(config):
+    result = yield
+    if result.get_result() == 0:
+        os._exit(0)
+    return result.get_result()
 
 
 CONTINUE_AS_NEW_SUGGEST_HISTORY_COUNT = 50
