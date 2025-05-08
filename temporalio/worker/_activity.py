e9084af0e2e671e47d9dd98c3865fecb244144f7
tconley1428 <tconley1428@gmail.com>
2025-05-08 14:42:46 -0700

    Update temporalio/worker/_activity.py
    Co-authored-by: Chad Retz <chad@temporal.io>

--- temporalio/worker/_activity.py
+++ temporalio/worker/_activity.py
@@ -533,9 +533,9 @@ class _ActivityWorker:
             temporalio.activity.logger.exception("Failed completing activity task")
 
     def assert_activity_valid(self, activity) -> Optional[str]:
-        activity_def = self._activities.get(activity)
         if self._dynamic_activity:
-            return None
+            return
+        activity_def = self._activities.get(activity)
         if not activity_def:
             activity_names = ", ".join(sorted(self._activities.keys()))
             raise ValueError(
