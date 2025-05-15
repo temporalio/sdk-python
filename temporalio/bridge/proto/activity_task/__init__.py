d03927cdd36d22ee9f21a888d5ee68efb01febe3
Spencer Judge <spencer@temporal.io>
2025-05-15 16:23:34 -0700

    Update protos

--- temporalio/bridge/proto/activity_task/__init__.py
+++ temporalio/bridge/proto/activity_task/__init__.py
@@ -1,7 +1,14 @@
-from .activity_task_pb2 import ActivityCancelReason, ActivityTask, Cancel, Start
+from .activity_task_pb2 import (
+    ActivityCancellationDetails,
+    ActivityCancelReason,
+    ActivityTask,
+    Cancel,
+    Start,
+)
 
 __all__ = [
     "ActivityCancelReason",
+    "ActivityCancellationDetails",
     "ActivityTask",
     "Cancel",
     "Start",
