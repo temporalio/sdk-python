fc4d6e1eedae9c4583d0789666294f4eda409c2d
Dan Davison <dan.davison@temporal.io>
2025-05-23 20:50:31 -0400

    Split test file

---
+++ tests/nexus/test_handler_interface_implementation.py
@@ -0,0 +1,64 @@
+from typing import Any, Optional, Type
+
+import nexusrpc
+import nexusrpc.handler
+import pytest
+
+import temporalio.api.failure.v1
+import temporalio.nexus
+from temporalio.client import WorkflowHandle
+
+HTTP_PORT = 7243
+
+
+class _InterfaceImplementationTestCase:
+    Interface: Type[Any]
+    Impl: Type[Any]
+    error_message: Optional[str]
+
+
+class ValidImpl(_InterfaceImplementationTestCase):
+    @nexusrpc.interface.service
+    class Interface:
+        op: nexusrpc.interface.Operation[None, None]
+
+    class Impl:
+        @nexusrpc.handler.sync_operation
+        async def op(
+            self, input: None, options: nexusrpc.handler.StartOperationOptions
+        ) -> None: ...
+
+    error_message = None
+
+
+class ValidWorkflowRunImpl(_InterfaceImplementationTestCase):
+    @nexusrpc.interface.service
+    class Interface:
+        op: nexusrpc.interface.Operation[str, int]
+
+    class Impl:
+        @temporalio.nexus.handler.workflow_run_operation
+        async def op(
+            self, input: str, options: nexusrpc.handler.StartOperationOptions
+        ) -> WorkflowHandle[Any, int]: ...
+
+    error_message = None
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        ValidImpl,
+        ValidWorkflowRunImpl,
+    ],
+)
+def test_service_decorator_enforces_interface_conformance(
+    test_case: Type[_InterfaceImplementationTestCase],
+):
+    if test_case.error_message:
+        with pytest.raises(Exception) as ei:
+            nexusrpc.handler.service(interface=test_case.Interface)(test_case.Impl)
+        err = ei.value
+        assert test_case.error_message in str(err)
+    else:
+        nexusrpc.handler.service(interface=test_case.Interface)(test_case.Impl)
