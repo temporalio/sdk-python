520aefd05366df35c7cfc693de31a356806db92a
tconley1428 <tconley1428@gmail.com>
2025-06-02 12:56:47 -0700

    Swallowed cancellation (#879)
    * Add SDK Flag for ensure cancellation is respected when an activity is completing
    
    * Linting
    
    * Revert "Linting"
    
    This reverts commit 898ed1d2c6b3245b5eb5025e33a8467559699230.
    
    * Linting
    
    * Remove log statements
    
    * Remove test until enabled outside of the replayer
    
    * Merge
    
    * Adding additional replay test, and a global override for a non-replay test
    
    * Address some test comments, fix sdk-core commit change

--- temporalio/worker/_workflow_instance.py
+++ temporalio/worker/_workflow_instance.py
@@ -17,6 +17,7 @@ from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from dataclasses import dataclass
 from datetime import timedelta
+from enum import IntEnum
 from typing import (
     Any,
     Awaitable,
@@ -367,8 +368,8 @@ class _WorkflowInstanceImpl(
         self._continue_as_new_suggested = act.continue_as_new_suggested
         self._time_ns = act.timestamp.ToNanoseconds()
         self._is_replaying = act.is_replaying
-
         self._current_thread_id = threading.get_ident()
+        self._current_internal_flags = act.available_internal_flags
         activation_err: Optional[Exception] = None
         try:
             # Split into job sets with patches, then signals + updates, then
@@ -1613,6 +1614,20 @@ class _WorkflowInstanceImpl(
                     # dict with its new seq
                     self._pending_activities[handle._seq] = handle
                 except asyncio.CancelledError:
+                    # If an activity future completes at the same time as a cancellation is being processed, the cancellation would be swallowed
+                    # _WorkflowLogicFlag.RAISE_ON_CANCELLING_COMPLETED_ACTIVITY will correctly reraise the exception
+                    if handle._result_fut.done():
+                        # TODO in next release, check sdk flag when not replaying instead of global override, remove the override, and set flag use
+                        if (
+                            (
+                                not self._is_replaying
+                                and _raise_on_cancelling_completed_activity_override
+                            )
+                            or _WorkflowLogicFlag.RAISE_ON_CANCELLING_COMPLETED_ACTIVITY
+                            in self._current_internal_flags
+                        ):
+                            # self._current_completion.successful.used_internal_flags.append(WorkflowLogicFlag.RAISE_ON_CANCELLING_COMPLETED_ACTIVITY)
+                            raise
                     # Send a cancel request to the activity
                     handle._apply_cancel_command(self._add_command())
 
@@ -3112,3 +3127,13 @@ finishes, then you can disable this warning via the signal handler decorator:
             [{"name": name, "count": count} for name, count in names.most_common()]
         )
     )
+
+
+class _WorkflowLogicFlag(IntEnum):
+    """Flags that may be set on task/activation completion to differentiate new from old workflow behavior."""
+
+    RAISE_ON_CANCELLING_COMPLETED_ACTIVITY = 1
+
+
+# Used by tests to validate behavior prior to SDK flag becoming default
+_raise_on_cancelling_completed_activity_override = False
