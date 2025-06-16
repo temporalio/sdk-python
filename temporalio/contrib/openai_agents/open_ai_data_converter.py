f39ed675f81e419783d10e4610d6d483fad52bd0
Tim Conley <timothy.conley@temporal.io>
2025-06-16 09:37:09 -0700

    Fix import

--- temporalio/contrib/openai_agents/open_ai_data_converter.py
+++ temporalio/contrib/openai_agents/open_ai_data_converter.py
@@ -5,7 +5,7 @@ These are mostly Pydantic types. Some of them should be explicitly imported.
 
 from __future__ import annotations
 
-from typing import Any, Optional, Type, TypeVar, Required
+from typing import Any, Optional, Required, Type, TypeVar
 
 from agents import Usage
 from agents.items import TResponseOutputItem
