625bec7468e9ea94617c05555d1d8d2d5a96d6a4
Dan Davison <dan.davison@temporal.io>
2025-06-25 08:59:42 -0400

    TODO

---
+++ temporalio/nexus/__init__.py
@@ -0,0 +1,3 @@
+# TODO(nexus-prerelease) WARN temporal_sdk_core::worker::nexus: Failed to parse nexus timeout header value '9.155416ms'
+# 2025-06-25T12:58:05.749589Z  WARN temporal_sdk_core::worker::nexus: Failed to parse nexus timeout header value '9.155416ms'
+# 2025-06-25T12:58:05.763052Z  WARN temporal_sdk_core::worker::nexus: Nexus task not found on completion. This may happen if the operation has already been cancelled but completed anyway. details=Status { code: NotFound, message: "Nexus task not found or already expired", details: b"\x08\x05\x12'Nexus task not found or already expired\x1aB\n@type.googleapis.com/temporal.api.errordetails.v1.NotFoundFailure", metadata: MetadataMap { headers: {"content-type": "application/grpc"} }, source: None }
