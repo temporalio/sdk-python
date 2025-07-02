09ae1874309c43fb505b01d3f7236ab3a9afbcdb
Dan Davison <dan.davison@temporal.io>
2025-07-02 19:24:51 -0400

    Revert change to callable types

--- temporalio/types.py
+++ temporalio/types.py
@@ -81,9 +81,8 @@ class MethodAsyncSingleParam(
 ):
     """Generic callable type."""
 
-    # TODO(nexus-prerelease)*: review changes to signatures in this file
     def __call__(
-        self, __self: ProtocolSelfType, __arg: ProtocolParamType
+        self, __self: ProtocolSelfType, __arg: ProtocolParamType, /
     ) -> Awaitable[ProtocolReturnType]:
         """Generic callable type callback."""
         ...
@@ -95,7 +94,7 @@ class MethodSyncSingleParam(
     """Generic callable type."""
 
     def __call__(
-        self, __self: ProtocolSelfType, __arg: ProtocolParamType
+        self, __self: ProtocolSelfType, __arg: ProtocolParamType, /
     ) -> ProtocolReturnType:
         """Generic callable type callback."""
         ...
@@ -117,7 +116,7 @@ class MethodSyncOrAsyncSingleParam(
     """Generic callable type."""
 
     def __call__(
-        self, __self: ProtocolSelfType, __param: ProtocolParamType
+        self, __self: ProtocolSelfType, __param: ProtocolParamType, /
     ) -> Union[ProtocolReturnType, Awaitable[ProtocolReturnType]]:
         """Generic callable type callback."""
         ...
