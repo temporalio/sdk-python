e80cdf72f466cb8d4aad8925bbee4c290131dc5f
Tim Conley <timothy.conley@temporal.io>
2025-08-07 12:41:06 -0700

    Delay import of mcp servers

--- temporalio/contrib/openai_agents/_temporal_openai_agents.py
+++ temporalio/contrib/openai_agents/_temporal_openai_agents.py
@@ -52,14 +52,10 @@ from temporalio.worker import (
 )
 from temporalio.worker.workflow_sandbox import SandboxedWorkflowRunner
 
-# MCP only supported on python >=3.10
-try:
-    from temporalio.contrib.openai_agents._mcp import TemporalMCPServer
-except ImportError:
-    pass
-
 if typing.TYPE_CHECKING:
     from agents.mcp import MCPServer
+
+    from temporalio.contrib.openai_agents._mcp import TemporalMCPServer
 
 
 @contextmanager
@@ -273,12 +269,18 @@ class OpenAIAgentsPlugin(temporalio.client.Plugin, temporalio.worker.Plugin):
         self._model_params = model_params
         self._model_provider = model_provider
 
-        self._mcp_servers = [
-            server
-            if isinstance(server, TemporalMCPServer)
-            else TemporalMCPServer(server)
-            for server in mcp_servers
-        ]
+        if mcp_servers:
+            # Delayed import as mcp servers only work on python >=3.10
+            from temporalio.contrib.openai_agents._mcp import TemporalMCPServer
+
+            self._mcp_servers = [
+                server
+                if isinstance(server, TemporalMCPServer)
+                else TemporalMCPServer(server)
+                for server in mcp_servers
+            ]
+        else:
+            self._mcp_servers = []
 
     def init_client_plugin(self, next: temporalio.client.Plugin) -> None:
         """Set the next client plugin"""
