12f4828621c897969fe14a29e73d3b5f01e3e66b
Tim Conley <timothy.conley@temporal.io>
2025-08-28 11:06:50 -0700

    Fix test for activity reset

--- temporalio/client.py
+++ temporalio/client.py
@@ -6363,12 +6363,16 @@ class _ClientImpl(OutboundInterceptor):
                 metadata=input.rpc_metadata,
                 timeout=input.rpc_timeout,
             )
-            if resp_by_id.cancel_requested or resp_by_id.activity_paused or resp_by_id.activity_reset:
+            if (
+                resp_by_id.cancel_requested
+                or resp_by_id.activity_paused
+                or resp_by_id.activity_reset
+            ):
                 raise AsyncActivityCancelledError(
                     details=ActivityCancellationDetails(
                         cancel_requested=resp_by_id.cancel_requested,
                         paused=resp_by_id.activity_paused,
-                        reset=resp_by_id.activity_reset
+                        reset=resp_by_id.activity_reset,
                     )
                 )
 
@@ -6389,6 +6393,7 @@ class _ClientImpl(OutboundInterceptor):
                     details=ActivityCancellationDetails(
                         cancel_requested=resp.cancel_requested,
                         paused=resp.activity_paused,
+                        reset=resp.activity_reset,
                     )
                 )
 
