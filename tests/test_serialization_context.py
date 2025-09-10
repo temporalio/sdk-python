1167da614e80ee5dc93327e781e1afc319f90454
Dan Davison <dan.davison@temporal.io>
2025-09-09 20:38:14 -0400

    Update test

--- tests/test_serialization_context.py
+++ tests/test_serialization_context.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import dataclasses
 import uuid
+from dataclasses import dataclass
 from typing import Any, Optional, Type
 
 from temporalio import workflow
@@ -10,27 +13,45 @@ from temporalio.converter import (
     DataConverter,
     DefaultPayloadConverter,
     EncodingPayloadConverter,
+    SerializationContext,
+    WithSerializationContext,
+    WorkflowSerializationContext,
 )
 from temporalio.worker import Worker
+
+
+@dataclass
+class WorkflowData:
+    workflow_context: Optional[WorkflowSerializationContext] = None
 
 
 @workflow.defn
 class SerializationContextTestWorkflow:
     @workflow.run
-    async def run(self, input: dict[str, Any]) -> dict[str, Any]:
+    async def run(self, input: WorkflowData) -> WorkflowData:
         return input
 
 
-class SerializationContextTestEncodingPayloadConverter(EncodingPayloadConverter):
+class SerializationContextTestEncodingPayloadConverter(
+    EncodingPayloadConverter, WithSerializationContext
+):
+    def __init__(self, context: Optional[SerializationContext]):
+        self.context = context
+
     @property
     def encoding(self) -> str:
         return "test-serialization-context"
 
+    def with_context(
+        self, context: Optional[SerializationContext]
+    ) -> SerializationContextTestEncodingPayloadConverter:
+        return SerializationContextTestEncodingPayloadConverter(context)
+
     def to_payload(self, value: Any) -> Optional[Payload]:
+        assert isinstance(value, WorkflowData)
+        assert isinstance(self.context, WorkflowSerializationContext)
+        value.workflow_context = self.context
         return None
-        # return Payload(
-        #     metadata={"encoding": self.encoding.encode()}, data=str(value).encode()
-        # )
 
     def from_payload(self, payload: Payload, type_hint: Optional[Type] = None) -> Any:
         raise RuntimeError("Not implemented")
@@ -40,7 +61,7 @@ class SerializationContextTestEncodingPayloadConverter(EncodingPayloadConverter)
 class SerializationContextTestPayloadConverter(CompositePayloadConverter):
     def __init__(self):
         super().__init__(
-            SerializationContextTestEncodingPayloadConverter(),
+            SerializationContextTestEncodingPayloadConverter(None),
             *DefaultPayloadConverter.default_encoding_payload_converters,
         )
 
@@ -54,17 +75,23 @@ data_converter = dataclasses.replace(
 async def test_workflow_payload_conversion_can_be_given_access_to_serialization_context(
     client: Client,
 ):
+    workflow_id = str(uuid.uuid4())
+    task_queue = str(uuid.uuid4())
+
     async with Worker(
         client,
-        task_queue="test-serialization-context",
+        task_queue=task_queue,
         workflows=[SerializationContextTestWorkflow],
         activities=[],
     ):
         result = await client.execute_workflow(
             SerializationContextTestWorkflow.run,
-            {},
-            id=f"test-serialization-context-{uuid.uuid4()}",
-            task_queue="test-serialization-context",
+            WorkflowData(),
+            id=workflow_id,
+            task_queue=task_queue,
         )
 
-        assert result == {}
+        assert result.workflow_context == WorkflowSerializationContext(
+            namespace="default",
+            workflow_id=workflow_id,
+        )
