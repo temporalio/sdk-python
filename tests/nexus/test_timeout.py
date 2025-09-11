13b8caef19ade841b552537e341c2b5e67adbded
Dan Davison <dan.davison@temporal.io>
2025-09-11 18:49:36 -0400

    Cleanup

--- tests/nexus/test_timeout.py
+++ tests/nexus/test_timeout.py
@@ -13,19 +13,27 @@ from tests.helpers import new_worker
 from tests.helpers.nexus import create_nexus_endpoint, make_nexus_endpoint_name
 
 
+@nexusrpc.handler.service_handler
+class NexusService:
+    @nexusrpc.handler.sync_operation
+    async def op_that_never_returns(
+        self, _ctx: nexusrpc.handler.StartOperationContext, id: None
+    ) -> None:
+        await asyncio.Event().wait()
+
+
 @workflow.defn
 class NexusCallerWorkflow:
-    """Workflow that calls a Nexus operation."""
-
     @workflow.run
     async def run(self) -> None:
         nexus_client = workflow.create_nexus_client(
             endpoint=make_nexus_endpoint_name(workflow.info().task_queue),
-            service="MaxConcurrentTestService",
+            service=NexusService,
         )
-
         await nexus_client.execute_operation(
-            "op", None, schedule_to_close_timeout=timedelta(seconds=60)
+            NexusService.op_that_never_returns,
+            None,
+            schedule_to_close_timeout=timedelta(seconds=10),
         )
 
 
@@ -33,18 +41,10 @@ async def test_nexus_timeout(env: WorkflowEnvironment):
     if env.supports_time_skipping:
         pytest.skip("Nexus tests don't work with Javas test server")
 
-    @nexusrpc.handler.service_handler
-    class MaxConcurrentTestService:
-        @nexusrpc.handler.sync_operation
-        async def op(
-            self, _ctx: nexusrpc.handler.StartOperationContext, id: None
-        ) -> None:
-            await asyncio.Event().wait()
-
     async with new_worker(
         env.client,
         NexusCallerWorkflow,
-        nexus_service_handlers=[MaxConcurrentTestService()],
+        nexus_service_handlers=[NexusService()],
     ) as worker:
         await create_nexus_endpoint(worker.task_queue, env.client)
 
