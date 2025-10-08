896fc8773d226b35ce3832ae5263690b8fec73f9
Dan Davison <dan.davison@temporal.io>
2025-10-08 04:06:52 -0400

    Test standalone activity

---
+++ tests/test_activity.py
@@ -0,0 +1,33 @@
+import uuid
+from datetime import timedelta
+
+from temporalio import activity
+from temporalio.client import Client
+from temporalio.common import ActivityExecutionStatus
+
+
+@activity.defn
+async def increment(input: int) -> int:
+    return input + 1
+
+
+async def test_describe_activity(client: Client):
+    activity_id = str("test_start_and_describe_activity_id")
+    task_queue = str(uuid.uuid4())
+
+    activity_handle = await client.start_activity(
+        increment,
+        args=(1,),
+        id=activity_id,
+        task_queue=task_queue,
+        start_to_close_timeout=timedelta(seconds=5),
+    )
+    desc = await activity_handle.describe()
+    assert desc.activity_id == activity_id
+    assert desc.run_id == activity_handle.run_id
+    assert desc.activity_type == "increment"
+    assert desc.task_queue == task_queue
+    assert desc.status in [
+        ActivityExecutionStatus.RUNNING,
+        ActivityExecutionStatus.COMPLETED,
+    ]
