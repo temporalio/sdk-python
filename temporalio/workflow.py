dbcbcb87e5091ae2f477cc57922e8b7bb06a1eb7
Thomas Hardy <thestaffofmoses@gmail.com>
2026-05-01 11:27:22 -0700

    CaN USE_RAMPING_VERSION versioning behaviour (#1499)

--- temporalio/workflow.py
+++ temporalio/workflow.py
@@ -5426,6 +5426,25 @@ class ContinueAsNewVersioningBehavior(IntEnum):
     effective version will be whatever is specified by the Versioning Override until the override is removed.
     """
 
+    USE_RAMPING_VERSION = int(
+        temporalio.api.enums.v1.ContinueAsNewVersioningBehavior.CONTINUE_AS_NEW_VERSIONING_BEHAVIOR_USE_RAMPING_VERSION
+    )
+    """Use the Ramping Version of the workflow's task queue at start time, regardless of the workflow's
+    Target Version. After the first workflow task completes, the workflow will use whatever Versioning
+    Behavior it is annotated with. If there is no Ramping Version by the time that the first workflow task
+    is dispatched, it will be sent to the Current Version.
+
+    It is highly discouraged to use this if the workflow is annotated with AutoUpgrade behavior, because
+    this setting ONLY applies to the first task of the workflow. If, after the first task, the workflow
+    is AutoUpgrade, it will behave like a normal AutoUpgrade workflow and go to the Target Version, which
+    may be the Current Version instead of the Ramping Version.
+
+    Note that if the workflow being continued has a Pinned override, that override will be inherited by the
+    new workflow run regardless of the ContinueAsNewVersioningBehavior specified in the continue-as-new
+    command. Versioning Override always takes precedence until it's removed manually via
+    UpdateWorkflowExecutionOptions.
+    """
+
 
 ServiceT = TypeVar("ServiceT")
 
