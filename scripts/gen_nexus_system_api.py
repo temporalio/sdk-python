1d248278c7a05c531585353a5d6eb4f9250ee880
Tim Conley <timothy.conley@temporal.io>
2026-08-03 10:17:40 -0700

    Pin nex-gen 0.2.1 for system API generation

--- scripts/gen_nexus_system_api.py
+++ scripts/gen_nexus_system_api.py
@@ -36,7 +36,7 @@ workflowservice_request_response_proto = (
     / "v1"
     / "request_response.proto"
 )
-NEX_GEN_VERSION = "0.2.0"
+NEX_GEN_VERSION = "0.2.1"
 
 
 def nex_gen_command() -> list[str]:
